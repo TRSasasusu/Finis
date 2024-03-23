0c2c22b9c731f412893023c88cdfb903019cd1ac
Kaito Kishi <17728957+TRSasasusu@users.noreply.github.com>
2024-03-24 01:23:07 +0900

    pick up rod

--- Finis/Patches.cs
+++ Finis/Patches.cs
@@ -4,9 +4,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 
 namespace Finis {
     [HarmonyPatch]
     public static class Patches {
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(OWItem), nameof(OWItem.MoveAndChildToTransform))]
+        public static void OWItem_MoveAndChildToTransform_Prefix(ref Transform socketTransform, OWItem __instance) {
+            if (socketTransform.parent.name != "ItemCarryTool") {
+                return;
+            }
+            if(__instance.name == "Rod") {
+                Finis.Log("Set Rod in RodSocket");
+                socketTransform = StateController.Instance.RodSocket;
+            }
+        }
     }
 }
--- Finis/StateController.cs
+++ Finis/StateController.cs
@@ -3,19 +3,60 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 using IEnumerator = System.Collections.IEnumerator;
 
 namespace Finis {
     public class StateController {
+        public static StateController Instance;
+
         public RodItem RodItem { get; private set; }
+        public Transform RodSocket { get; private set; }
+
+        GameObject _finisPlateauSector;
+
+        public StateController() {
+            Instance = this;
+        }
 
         public void Initialize() {
             Finis.Instance.StartCoroutine(InitializeBody());
         }
 
         IEnumerator InitializeBody() {
+            Finis.Log("Start InitializeBody");
+
+            GameObject defaultItemSocket;
             while (true) {
                 yield return null;
+                defaultItemSocket = GameObject.Find("Player_Body/PlayerCamera/ItemCarryTool/ItemSocket");
+                if (defaultItemSocket) {
+                    break;
+                }
+            }
+            RodSocket = GameObject.Instantiate(defaultItemSocket).transform;
+            RodSocket.gameObject.name = "RodSocket";
+            RodSocket.parent = defaultItemSocket.transform.parent;
+            RodSocket.localPosition = new Vector3(0.325f, -0.22f, 0.4f);
+            RodSocket.localEulerAngles = new Vector3(0, 340, 25);
+            Finis.Log("Set RodSocket");
+
+            while (true) {
+                yield return null;
+                _finisPlateauSector = GameObject.Find("FinisPlateau_Body/Sector");
+                if(_finisPlateauSector) {
+                    break;
+                }
+            }
+            Finis.Log("Found our sector");
+
+            foreach(var child in _finisPlateauSector.GetComponentsInChildren<Transform>()) {
+                if(child.name == "Rod") {
+                    var rodItem = child.gameObject.AddComponent<RodItem>();
+                    rodItem._localDropOffset = new Vector3(0, 1, 0);
+                    child.gameObject.AddComponent<OWCollider>();
+                    Finis.Log("Set RodItem");
+                }
             }
         }
     }
