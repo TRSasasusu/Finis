cfb499a797c116339f9fab0a7bc544ad0237f6ac
Kaito Kishi <17728957+TRSasasusu@users.noreply.github.com>
2024-10-09 00:59:05 +0900

    add emission on pictures and add props near the pod

--- Finis/FixOWObj.cs
+++ Finis/FixOWObj.cs
@@ -14,6 +14,8 @@ namespace Finis {
 
         const string PLANET_SPHERE_PATH = "FinisPlateau_Body/Sector/finis_plateau/Sphere";
         const string WHITE_BOARD_PATH = "FinisPlateau_Body/Sector/Prefab_NOM_Whiteboard (1)/Props_NOM_Whiteboard (1)";
+        const string TABLE_PATH = "FinisPlateau_Body/Sector/Props_NOM_Table_Geo";
+        const string SHELF_PATH = "FinisPlateau_Body/Sector/Strucutre_NOM_Shelf";
 
         Material _grayMat;
 
@@ -65,6 +67,40 @@ namespace Finis {
                 }
             }
             Finis.Log("end: finding whiteboard");
+
+            Finis.Log("start: finding table");
+            while(true) {
+                yield return null;
+                var table = GameObject.Find(TABLE_PATH);
+                if(table) {
+                    table.AddComponent<BoxCollider>();
+
+                    var tableRenderer = table.GetComponent<MeshRenderer>();
+                    var tableMats = tableRenderer.materials;
+                    tableMats[0] = _grayMat;
+                    tableMats[2] = _grayMat;
+                    tableRenderer.materials = tableMats;
+                    break;
+                }
+            }
+            Finis.Log("end: finding table");
+
+            Finis.Log("start: finding shelf");
+            while(true) {
+                yield return null;
+                var shelf = GameObject.Find(SHELF_PATH);
+                if(shelf) {
+                    shelf.AddComponent<BoxCollider>();
+
+                    var shelfRenderer = shelf.GetComponent<MeshRenderer>();
+                    var shelfMats = shelfRenderer.materials;
+                    shelfMats[0] = _grayMat;
+                    shelfMats[1] = _grayMat;
+                    shelfRenderer.materials = shelfMats;
+                    break;
+                }
+            }
+            Finis.Log("end: finding shelf");
         }
     }
 }
